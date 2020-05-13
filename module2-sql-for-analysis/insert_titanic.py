d46654aa327cee060cc2d8539b947594935071d6
shengjiyang <samswank@gmail.com>
2020-05-13 15:52:23 -0600

    Second Commit

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -1,13 +1,19 @@
 # module2-sql-for-analysis/insert_titanic.py
 
 # Read Passenger Data From the CSV File
+import psycopg2
+from psycopg2.extensions import register_adapter, AsIs
+from psycopg2.extras import execute_values
+import numpy as np
 import os
 from dotenv import load_dotenv
 import pandas as pd
-import psycopg2 as psycho
-from psycopg2.extras import execute_values
 import json
 
+# Ensures that psycopg2 and by extension ElephantSQL
+# understand the np.int64 values from the Titanic
+# DataFrame
+register_adapter(np.int64, psycopg2._psycopg.AsIs)
 
 CSV_FILEPATH = os.path.join(os.path.dirname(__file__),
                             "..", 
@@ -15,9 +21,10 @@ CSV_FILEPATH = os.path.join(os.path.dirname(__file__),
                             "titanic.csv")
 
 df = pd.read_csv(CSV_FILEPATH)
-print(df.dtypes)
+# Ensures that SQL's PRIMARY KEY and DataFrame's Index
+# match up
+df.index += 1
 print(df.head())
-
 
 # Connect to PostgreSQL DataBase
 
@@ -29,7 +36,7 @@ DB_PW = os.getenv("DB_PW", "Invalid DB_PW value")
 DB_HOST = os.getenv("DB_HOST", "Invalid DB_HOST value")
 
 
-connection = psycho.connect(dbname=DB_NAME,
+connection = psycopg2.connect(dbname=DB_NAME,
                             user=DB_USER,
                             password=DB_PW,
                             host=DB_HOST)
@@ -47,6 +54,7 @@ cur = connection.cursor()
 insert_table = """
 CREATE TABLE IF NOT EXISTS titanic(
 
+ID                       SERIAL PRIMARY KEY,
 SURVIVED                 INTEGER NOT NULL,
 PCLASS                   INTEGER NOT NULL,
 NAME                     TEXT NOT NULL,
@@ -59,6 +67,19 @@ FARE                     NUMERIC NOT NULL
 """
 
 cur.execute(insert_table)
+connection.commit()
 
 # Insert Data Into Passengers Table
+records = df.to_records(index="True")
+titanic_data = list(records)
+print(len(titanic_data))
 
+insert_data = """
+INSERT INTO titanic
+(ID, SURVIVED, PCLASS, NAME, SEX, AGE,
+SIBLINGS_SPOUSES_ABOARD, PARENTS_CHILDREN,
+FARE) VALUES%s
+"""
+
+execute_values(cur, insert_data, titanic_data)
+connection.commit()
